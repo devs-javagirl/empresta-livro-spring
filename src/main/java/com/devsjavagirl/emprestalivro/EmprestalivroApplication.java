da1bed649f40eff3ae7dc08bb200f4a6716fa0dd
Nara Sakamoto <nara.sakamoto@moip.com.br>
2019-08-22 13:05:58 -0300

    Criando um esqueleto do projeto

---
+++ src/main/java/com/devsjavagirl/emprestalivro/EmprestalivroApplication.java
@@ -0,0 +1,13 @@
+package com.devsjavagirl.emprestalivro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprestalivroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprestalivroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsjavagirl/emprestalivro/EmprestalivroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devsjavagirl.emprestalivro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmprestalivroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
