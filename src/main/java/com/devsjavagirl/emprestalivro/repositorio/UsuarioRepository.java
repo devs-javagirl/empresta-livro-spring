ac1e2a9b76da0b9f6aa641e11ff59bc801d0e51f
Nara Sakamoto <nara.sakamoto@moip.com.br>
2019-08-22 13:24:21 -0300

    Criando a interface UsuarioRepository

--- src/main/java/com/devsjavagirl/emprestalivro/repositorio/UsuarioRepository.java
+++ src/main/java/com/devsjavagirl/emprestalivro/repositorio/UsuarioRepository.java
@@ -1,4 +1,7 @@
 package com.devsjavagirl.emprestalivro.repositorio;
 
-public interface UsuarioRepository {
+import com.devsjavagirl.emprestalivro.dominio.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
