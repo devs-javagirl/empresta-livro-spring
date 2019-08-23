e9e64aaa4b4ce3c857552d1bc2ba77caffd8414c
Nara Sakamoto <nara.sakamoto@moip.com.br>
2019-08-23 00:36:38 -0300

    Criando classe EmprestimoController

---
+++ src/main/java/com/devsjavagirl/emprestalivro/controller/EmprestimoController.java
@@ -0,0 +1,20 @@
+package com.devsjavagirl.emprestalivro.controller;
+
+import com.devsjavagirl.emprestalivro.dominio.Emprestimo;
+import com.devsjavagirl.emprestalivro.servico.EmprestimoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmprestimoController {
+
+    @Autowired
+    private EmprestimoService emprestimoService;
+
+    @PostMapping("/emprestimo")
+    public void save(@RequestBody Emprestimo emprestimo){
+        emprestimoService.save(emprestimo);
+    }
+}
