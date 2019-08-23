1c4fa562f7ffc24af04705d11e1c4b75af96cf66
Nara Sakamoto <nara.sakamoto@moip.com.br>
2019-08-23 00:30:02 -0300

    Criando entidade Emprestimo

---
+++ src/main/java/com/devsjavagirl/emprestalivro/dominio/Emprestimo.java
@@ -0,0 +1,43 @@
+package com.devsjavagirl.emprestalivro.dominio;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+public class Emprestimo {
+
+    @Id
+    @GeneratedValue
+    private Long codigo;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @ManyToOne
+    private Livro livro;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Livro getLivro() {
+        return livro;
+    }
+
+    public void setLivro(Livro livro) {
+        this.livro = livro;
+    }
+}
